cf8f2e430734d8812df14e64154a299a10e20e8f
TEnQian <88424172+TEnQian@users.noreply.github.com>
2024-01-30 16:59:31 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+const targetChannelID = '1199903534456504431';
+const targetUserID = '548140081030889472';
+
+var channel_members;
+var channel;
+
+client.on(Events.ClientReady, readyClient => {
+    channel = client.channels.cache.get(targetChannelID);
+    var targetChanel = client.channels.cache.get(targetChannelID);
+    channel_members = targetChanel.members;
+});
+
+
+client.on(Events.MessageCreate, (msg) => {
+    var msgUserName = msg.author.username;
+    var channelName = channel.name;
+    var serverName = channel.guild.name;
+    var sticker = msg.stickers.size;
+    var msgAttachment = msg.attachments;
+    var attachmentURL = '';
+
+    if(msgAttachment){
+        for(let attachItem of msgAttachment) {
+            attachmentURL += '\r\n'+ attachItem[1].url;
+        }
+    }
+
+    var noticeMSG = "New Messages : " + msg.content + "\r\nFrom user : **" + msgUserName +"** at channel **" + channelName + "** (Server : " + serverName + ")";
+
+    if(attachmentURL){
+        noticeMSG = "Image upload from **" + msgUserName + "** at channel **" + channelName + "** (Server : " + serverName + ")" + "\r\n" + attachmentURL;
+    }
+
+    if(sticker === 1){
+        return; //Return if sent message is sticker, Discord bot unable to send sticker
+    }
+    
+    client.users.send(targetUserID,noticeMSG);
+ });
+
+
+client.login(token);
